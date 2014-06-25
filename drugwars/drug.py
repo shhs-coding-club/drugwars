11df94d9b37d35d6032d2054eb2eb48218772a0c
katiebrennan <matthewbrennan@new-host-4.home>
2014-06-25 15:26:42 -0400

    Adds general drug class

---
+++ drugwars/drug.py
@@ -0,0 +1,7 @@
+class Drug:
+	"""This is the general class for drugs, the parameters are as follows"""
+
+	def _init_(self, nm='Drug'):
+        self.name = nm
+        print'Created a class for', nm
+        
