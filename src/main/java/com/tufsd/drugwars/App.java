e6ddbaa3c2aad772ae4d2a080d5750146e4a682c
unknown <dkim@SH301NPC9.TUFSD.local>
2014-09-24 15:57:57 -0400

    Add UI class; create splashscreen method

--- src/main/java/com/tufsd/drugwars/App.java
+++ src/main/java/com/tufsd/drugwars/App.java
@@ -1,28 +1,10 @@
-package com.tufsd.drugwars;
+package src.main.java.com.tufsd.drugwars;
 import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        String logo = " ____                    __        __             \n"+
-                      "|  _ \\ _ __ _   _  __ _  \\ \\      / /_ _ _ __ ___ \n"+
-                      "| | | | '__| | | |/ _` |  \\ \\ /\\ / / _` | '__/ __|\n"+
-                      "| |_| | |  | |_| | (_| |   \\ V  V / (_| | |  \\__ \\\n"+
-                      "|____/|_|   \\__,_|\\__, |    \\_/\\_/ \\__,_|_|  |___/\n"+
-                      "                  |___/                           \n"+
-                      "                   ";
-
-        String creds = "From the Sleepy Hollow High School Coding Club!\n"+
-                       "Presidents: Colin Atkinson, Katie Brennan\n"+
-                       "Advisor: Daniel Larkin\n"+
-                       "https://github.com/shhs-coding-club/drugwars\n";
-
-        System.out.println(logo);
-        System.out.println(creds);
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Press return to continue...");  
-        scanner.nextLine();
+        UI.splashscreen();
     }
 }
