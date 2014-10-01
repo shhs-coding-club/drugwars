0e03a808c39ffdca091537bfb04d07657c931ebf
Abigail Banting <byzantine1221@gmail.com>
2014-10-01 16:30:12 -0400

    Add menu screen and javadoc to UI

--- src/main/java/com/tufsd/drugwars/UI.java
+++ src/main/java/com/tufsd/drugwars/UI.java
@@ -1,13 +1,22 @@
-package com.tufsd.drugwars;
+package src.main.java.com.tufsd.drugwars;
 import java.util.Scanner;
 
 
-/** Drug Wars interface made by Drug Lords Abigail, Amanda, Daniel, Dong Kyu, Jack
+/** 
+ * Drug Wars interface made by Drug Lords Abigail, Amanda, Daniel, Dong Kyu, Jack, Jason (Team BADASSNESS!!!!!!)
+ * ----------------------------------------------------------
+ * UI method implements introduction splashscreen badassness
+ * ----------------------------------------------------------
  */
 public class UI
 {
+    /**
+     * ------------------------------------------
+     * Function splashcreen shows Drug Wars logo
+     * ------------------------------------------
+     */
      public static void splashscreen()
-    {   //Splashscreen of Drug Wars
+    {   
         String logo = " ____                    __        __             \n"+
                       "|  _ \\ _ __ _   _  __ _  \\ \\      / /_ _ _ __ ___ \n"+
                       "| | | | '__| | | |/ _` |  \\ \\ /\\ / / _` | '__/ __|\n"+
@@ -27,5 +36,47 @@ public class UI
         Scanner scanner = new Scanner(System.in);
         System.out.print("Press return to continue...");  
         scanner.nextLine();
+        startScreen();
     }
+    /**
+     * -------------------------------------------------------------
+     * Function dummyFunction serves as placehold for future coding
+     * -------------------------------------------------------------
+     */
+    public static void dummyFunction()
+    {
+        System.out.println ("dummyFunction execute");
+    }
+    
+    /**
+     * ---------------------------------------------------------------------------------------
+     * Function startScreen sends command for 1 and 2 to begin new game or quit, respectively
+     * ---------------------------------------------------------------------------------------
+     */
+    public static void startScreen ()
+    {
+        System.out.println ("Press 1 to begin new game");
+        
+        System.out.println ("Press 2 to quit");
+        
+        Scanner menuScanner = new Scanner (System.in);
+        int menuChoice;
+        menuChoice = menuScanner.nextInt();
+        
+        System.out.println (menuChoice);
+        if (menuChoice == 1)
+        {
+            dummyFunction();
+        }
+        else if (menuChoice == 2)
+        {
+            System.exit(0);
+        }
+        else
+        {
+            System.out.println ("Unknown Option");
+            System.exit(0);
+        }
+    }
+        
 }
